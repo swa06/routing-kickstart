787c5e1552f3602d41d0c20ec70ee41809f85369
syed.ahmad <leowashfi@gmail.com>
2017-07-16 16:33:14 +0600

    routing commit without node_modules

---
+++ src/app/app.routing.ts
@@ -0,0 +1,15 @@
+
+import {WeatherComponent} from "./weather/weather.component";
+import {MovieComponent} from "./movie/movie.component";
+import {CurrencyComponent} from "./currency/currency.component";
+import {Routes, RouterModule} from "@angular/router";
+
+
+const MAINMENU_ROUTES:Routes = [
+    {path: '', redirectTo: '/weather', pathMatch: 'full'},
+    {path: 'weather', component: WeatherComponent},
+    {path: 'movie', component: MovieComponent},
+    {path: 'currency', component: CurrencyComponent}
+];
+
+export const CONST_ROUTING = RouterModule.forRoot(MAINMENU_ROUTES);
